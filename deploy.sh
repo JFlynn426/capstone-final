560330759ae32987181aa8dab63b0793889dcb72
John Flynn <jflynnpics@gmail.com>
2019-01-18 12:13:55 -0500

    updated gitignore

--- deploy.sh
+++ deploy.sh
@@ -2,13 +2,13 @@ dotnet publish -c Release
 
 cp dockerfile ./bin/release/netcoreapp2.2/publish
 
-docker build -t sdg-template-image ./bin/release/netcoreapp2.2/publish
+docker build -t johnflynnphotos-image ./bin/release/netcoreapp2.2/publish
 
-docker tag sdg-template-image registry.heroku.com/heroku-web-app/web
+docker tag johnflynnphotos-image registry.heroku.com/johnflynnphotos/web
 
-docker push registry.heroku.com/heroku-web-app/web
+docker push registry.heroku.com/johnflynnphotos/web
 
-heroku container:release web -a heroku-web-app
+heroku container:release web -a johnflynnphotos
 
 # sudo chmod 755 deploy.sh
 # ./deploy.sh
